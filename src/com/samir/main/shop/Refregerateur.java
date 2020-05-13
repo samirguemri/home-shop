cb839b5fe95d6d273f7e243f981083b2724d5668
Samir Guemri <samir.guemri@gmx.fr>
2020-05-13 09:06:20 +0200

    Ajout de Televiseur & Congelateur

---
+++ src/com/samir/main/shop/Refregerateur.java
@@ -0,0 +1,28 @@
+package com.samir.main.shop;
+
+public class Refregerateur extends Produit{
+    private int capacite;
+    private boolean congelateur;
+
+    /**
+     * Constructeur d'un Produit
+     *  @param nom      Le nom du Produit
+     * @param prix     Le prix du Produit
+     * @param quantite La quantite disponibe
+     * @param capacite La capasite du Refregerateur en Litre
+     * @param congelateur est il congelateur
+     */
+    public Refregerateur(String nom, float prix, int quantite, int capacite, boolean congelateur) {
+        super(nom, prix, quantite);
+        this.capacite = capacite;
+        this.congelateur = congelateur;
+    }
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public boolean isCongelateur() {
+        return congelateur;
+    }
+}
---
+++ src/com/samir/main/shop/Televiseur.java
@@ -0,0 +1,29 @@
+package com.samir.main.shop;
+
+public class Televiseur extends Produit{
+    private int tailleEcran;
+    private String typeEcran;
+
+    /**
+     * Constructeur d'un Televiseur
+     *
+     * @param nom      Le nom du Produit
+     * @param prix     Le prix du Produit
+     * @param quantite La quantite disponibe
+     * @param tailleEcran LA taille de l'ecran en pouce
+     * @param typeEcran Le type de l'ecran
+     */
+    public Televiseur(String nom, float prix, int quantite, int tailleEcran, String typeEcran) {
+        super(nom, prix, quantite);
+        this.tailleEcran = tailleEcran;
+        this.typeEcran = typeEcran;
+    }
+
+    public int getTailleEcran() {
+        return tailleEcran;
+    }
+
+    public String getTypeEcran() {
+        return typeEcran;
+    }
+}
